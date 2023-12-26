3a7107fbc6a440e25bb5a14df4fda1fde3c6873f
Drishya <drishya.pou@gmail.com>
2023-12-26 20:02:42 +0545

    first push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import PricingPage from "./routes/PricingPage";
+import FaqPage from "./routes/FaqPage";
+import ContactPage from "./routes/ContactPage";
+
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/pricing' element={<PricingPage/>} />
+                <Route path='/faq' element={<FaqPage/>} />
+                <Route path='/contact' element={<ContactPage/>} />
+            </Routes>
+        </>
+    );
+};
+
+export default App;
